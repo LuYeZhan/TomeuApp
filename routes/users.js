462c27f513ec269d84cc437f0815e1ad689db160
Lu <lu@hooli.store>
2019-07-27 17:14:19 +0200

    link to profile fixed, hemos descomentado una linea, hemos arreglado el form

--- routes/users.js
+++ routes/users.js
@@ -7,9 +7,13 @@ const User = require('../models/User.js');
 
 /* GET users listing. */
 router.get('/profile', async (req, res, next) => {
-  const userId = req.session.currentUser._id;
-  // const user = await User.findById(userId).populate('recipes');
-  res.render('profile', user);
+  try {
+    const userId = req.session.currentUser._id;
+    const user = await User.findById(userId).populate('events');
+    res.render('profile', user);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
