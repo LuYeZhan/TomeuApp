74d6dcfdf776caa79d031dba4ba9faba4e8392b0
Jundi Ye <rundiye93@gmail.com>
2019-07-28 13:39:55 +0200

    added edit view

--- routes/events.js
+++ routes/events.js
@@ -25,7 +25,37 @@ router.post('/create', async (req, res, next) => {
     const eventId = event._id;
     const userId = req.session.currentUser._id;
     await User.findByIdAndUpdate(userId, { $push: { events: eventId } });
-    res.redirect('/');
+    res.redirect('/users/profile');
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/:eventId/edit', async (req, res, next) => {
+  const eventId = req.params.eventId;
+  try {
+    const event = await Event.findById(eventId);
+    res.render('/events/edit', event);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/:eventId', async (req, res, next) => {
+  const eventId = req.params.eventId;
+  const { title, location, date, duration, attendees, description, menu } = req.body;
+  const newEvent = {
+    title,
+    location,
+    date,
+    duration,
+    attendees,
+    description,
+    menu
+  };
+  try {
+    await Event.findByIdAndUpdate(eventId, newEvent);
+    res.redirect('/users/profile');
   } catch (error) {
     next(error);
   }
